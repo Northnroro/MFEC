61f9928e9480ee50de50b04f571ef1e0cadc28a3
Northnroro <northnroro@gmail.com>
2016-05-25 01:47:06 +0700

    Remove unused imports.

--- src/WageCalculator.java
+++ src/WageCalculator.java
@@ -1,13 +1,7 @@
 import java.io.File;
 import java.io.FileNotFoundException;
-import java.text.DateFormat;
-import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.Calendar;
-import java.util.Comparator;
-import java.util.Date;
 import java.util.HashMap;
-import java.util.Map;
 import java.util.Scanner;
 import java.util.TreeMap;
 import java.util.TreeSet;
